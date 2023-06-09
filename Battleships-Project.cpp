4c2b18858d6b51194e380b9cd9e98635c4cf26b3
MatheusAlmeida909 <almeidasmatheus909@gmail.com>
2023-06-09 19:52:01 -0300

    Definição de Requisitos do jogo

--- Battleships-Project.cpp
+++ Battleships-Project.cpp
@@ -1,16 +1,18 @@
 /*		Requisitos funcionais
 * 
 *	[ ] Matris 10x10 para campo de jogo
+*	[ ] Matris 10x10 para respostas 
 *	[ ] Geração dos alvos no campo para serem encontrados
 *	[ ] Aleatorizar a posição dos alvos no campo de forma ordenada
 *	[ ] Recebeer 2 valores do jogador para indicar qual posição do campo atacar
-*	[ ] Processar os dois valores e traduzir para pontos no campo
+*	[ ] Traduzir os dois valores para pontos no campo de jogo e campo de resposta
+*	[ ] Comparar posições de no campo principal e de resposta para confirmar um acerto ou erro
 *	[ ] Contagem de acertos e erros de jogador
 *	{ } Interface de menu principal
 *	{ } Salvamento de pontuação
 *	{ } Tabela das 10 pontuações mais altas
 *	{ } Salvar iniciais do jogador para fácil distinção na tabela de pontuação
-*	{ }
+*	{ }	
 * 
 *	[ ] = requisito ainda a ser cumprido
 *	[x] = requisito cumprido
