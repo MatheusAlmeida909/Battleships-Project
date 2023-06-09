63d0af91c7cd2f559bae6ccfd4ddcc2b7976a826
MatheusAlmeida909 <almeidasmatheus909@gmail.com>
2023-06-09 19:45:11 -0300

    Adicionar arquivos de projeto.

---
+++ Battleships-Project.cpp
@@ -0,0 +1,19 @@
+/*		Requisitos funcionais
+* 
+*	[ ] Matris 10x10 para campo de jogo
+*	[ ] Geração dos alvos no campo para serem encontrados
+*	[ ] Aleatorizar a posição dos alvos no campo de forma ordenada
+*	[ ] Recebeer 2 valores do jogador para indicar qual posição do campo atacar
+*	[ ] Processar os dois valores e traduzir para pontos no campo
+*	[ ] Contagem de acertos e erros de jogador
+*	{ } Interface de menu principal
+*	{ } Salvamento de pontuação
+*	{ } Tabela das 10 pontuações mais altas
+*	{ } Salvar iniciais do jogador para fácil distinção na tabela de pontuação
+*	{ }
+* 
+*	[ ] = requisito ainda a ser cumprido
+*	[x] = requisito cumprido
+*	{ } = requisito opcional ainda a ser alcançado
+*	{x} = requisito opcional cumprido
+*/
